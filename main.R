31ffd4252d5033bf6e41f30a49e2c2f1881ad2b3
Xime1909 <134742374+Xime1909@users.noreply.github.com>
2023-05-29 21:21:53 -0500

    Add files via upload

---
+++ main.R
@@ -0,0 +1,45 @@
+library(readxl)
+VINO_BLANCO_MEDIAS <- read_excel("~/Trabajo Estadistica/VINO BLANCO MEDIAS.xlsx")
+View(VINO_BLANCO_MEDIAS)
+attach(VINO_BLANCO_MEDIAS)
+summary(VINO_BLANCO_MEDIAS) #Resumen de los datos
+resumen_estadistico<-basicStats(VINO_BLANCO_MEDIAS[,-1])
+resumen_estadistico
+summary(VINO_BLANCO_MEDIAS) #Resumen de los datos
+resumen_estadistico<-basicStats(VINO_BLANCO_MEDIAS[,-2])
+resumen_estadistico
+attach(VINO_BLANCO_MEDIAS)
+str(VINO_BLANCO_MEDIAS) # Estructura de los datos
+dim(VINO_BLANCO_MEDIAS)
+summary(VINO_BLANCO_MEDIAS) #Resumen de los datos
+resumen_estadistico<-basicStats(VINO_BLANCO_MEDIAS[,-2])
+resumen_estadistico<-basicStats(VINO_BLANCO_MEDIAS[,-1])
+resumen_estadistico
+resumen_estadistico<-basicStats(VINO_BLANCO_MEDIAS[,-1])
+install.packages("FinCal")
+install.packages("FinCal")
+library(FinCal)
+#Forma de una distribucion: simetría, curtosis y normalidad
+install.packages("psych")
+library(psych)
+#Instalamos la libreria "fBasics" para hallar Estadisticos básicos
+install.packages("fBasics")
+library(fBasics) #
+attach(VINO_BLANCO_MEDIAS)
+str(VINO_BLANCO_MEDIAS) # Estructura de los datos
+dim(VINO_BLANCO_MEDIAS)
+summary(VINO_BLANCO_MEDIAS) #Resumen de los datos
+resumen_estadistico<-basicStats(VINO_BLANCO_MEDIAS[,-1])
+resumen_estadistico
+resumen_estadistico<-basicStats(VINO_BLANCO_MEDIAS[,-1])
+library(readxl)
+VINO_TINTO_MEDIAS <- read_excel("~/Trabajo Estadistica/VINO TINTO MEDIAS.xlsx")
+View(VINO_TINTO_MEDIAS)
+attach(VINO_BLANCO_MEDIAS)
+str(VINO_TINTO_MEDIAS) # Estructura de los datos
+dim(VINO_TINTO_MEDIAS)
+summary(VINO_TINTO_MEDIAS) #Resumen de los datos
+resumen_estadistico<-basicStats(VINO_TINTO_MEDIAS[,-1])
+resumen_estadistico
+resumen_estadistico<-basicStats(VINO_TINTO_MEDIAS[,-1])
+summary(VINO_TINTO_MEDIAS) #Resumen de los datos
